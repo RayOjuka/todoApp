8e57a33ae9eb36403360c549bda72cc81881d186
Ray Ojuka <ray@pathwaysinternational.com>
2022-12-13 08:29:07 +0300

    Added todo functionality to the app

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig:{
+        apiKey: "AIzaSyCSm8nsZXFR0QugkQI6PBVg7YWUglB4fxk",
+        authDomain: "todo-app-ff72a.firebaseapp.com",
+        projectId: "todo-app-ff72a",
+        storageBucket: "todo-app-ff72a.appspot.com",
+        messagingSenderId: "185000996970",
+        appId: "1:185000996970:web:f589cc5b85a16d049e9a13"
+    }
+};
